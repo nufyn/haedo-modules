536933fc342d6094042921fd81ddc2ca738e0c0f
정다운 <nsadfhs@gmail.com>
2022-08-24 07:27:32 +0900

    2022.08.24 좀 더 많은 것들이 추가되고 종류가 만힝 생김

--- template.js
+++ template.js
@@ -1,92 +1,86 @@
-// // Create a class for the element
-class Content extends HTMLElement {
+/**
+ * 비상속 element
+ */
+
+class MyElement extends HTMLElement {
+    constructor() {
+        super();
+        // element created
+    }
+
     connectedCallback() {
+        // browser calls this method when the element is added to the document
+        // (can be called many times if an element is repeatedly added/removed)
         // ------- Start Lock --------
         // Element 태그의 속성 값 JSON 만들기
-        let Attributes = new Object;
+        let attrObj = new Object;
         // console.log(this.getAttributeNames());
         for (var i = 0; i < this.getAttributeNames().length; i++) {
-            Attributes[this.getAttributeNames()[i]] = this.getAttribute(this.getAttributeNames()[i]);
+            let attribute = this.getAttributeNames()[i];
+            console.log(attribute);
+            attrObj[attribute] = this.getAttribute(attribute);
         }
-        // console.log(Attributes);
-        this.setModule(this.setElement(Attributes), this.setStyle(Attributes));
+
+        this.setModule(this.setElement(attrObj), this.setStyle(attrObj));
         this.setEvent();
         // ------- Finish Lock --------
     }
+
     setModule(moduleElement, moduleStyle) {
-        let thisModuleElement = moduleElement;
-        let thisModuleStyle = `<style>${moduleStyle}</style>`;
-        let thisModule = thisModuleElement + thisModuleStyle;
+        console.log(moduleElement);
+        console.log(moduleStyle);
+
+        // let thisModuleElement = moduleElement;
+        // let thisModuleStyle = `<style>${moduleStyle}</style>`;
+        // let thisModule = thisModuleElement + thisModuleStyle;
         this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = thisModule;
+        // this.shadowRoot.innerHTML = "";
+        this.shadowRoot.append(moduleElement, moduleStyle);
     }
+
     // Element 처리부
     setElement(params) {
-        // console.log(JSON.parse(params.links));
 
+        let { } = params;
 
+        let childObj = new Object;
+        for (var i = 0; i < this.children.length; i++) {
+            let child = this.children[i];
+            console.log(child); // 10, 20, 30 출력
+            childObj[child.localName] = child
+        }
+        console.log(this.children)
+        console.log(childObj);
 
-        return `
-            <article>
+        const $wrapper = document.createElement('div');
 
-            </article>
-        `;
+        return $wrapper;
     }
-    // Style 처리부
+
     setStyle(params) {
 
-        let { color, bgcolor, size } = params;
-        let padding, margin = 0;
+        let { } = params;
 
-        switch (size) {
-            case "small || sm":
-                padding = "3px 8px";
-            case "middle || md":
-                padding = "8px 16px";;
-            case "lagre || lg":
-                padding = "8px 32px";
-        }
-
-        if (size == "small") {
-            padding = "3px 8px";
-        } else {
-
-        }
-
-        if (!color) {
-            color = "black";
-        }
-
-        if (!bgcolor) {
-            bgcolor = "white";
-        }
-
-        return `
-        * {
-            box-sizing:border-box;
-            color: inherit;
-            font: inherit;
-            padding: 0;
-            margin: 0;
-            border: none;
-            outline: 0;
-            text-decoration: 0;
-            list-style: 0;
-        }
-    
-        h1 {
-            font-size: 24px;
-            line-height: 1.5;
-        }
-
-        ellipsis {
-            display: inline-block;
-            overflow:hidden;
-            text-overflow:ellipsis;
-            white-space:nowrap;
-        }
+        let styleString = `
+            * {
+                box-sizing:border-box;
+                color: inherit;
+                font: inherit;
+                padding: 0;
+                margin: 0;
+                border: none;
+                outline: 0;
+                text-decoration: 0;
+                list-style: 0;
+            }
         `;
+
+        const $styledElem = document.createElement("style");
+        $styledElem.textContent = styleString;
+
+        return $styledElem;
     }
+
     // Event 처리부
     setEvent() {
         // 이벤트 트리거는 'this.shadowRoot'밑에서 찾는다.
@@ -94,80 +88,47 @@ class Content extends HTMLElement {
         // const _form = this.getAttribute("form"); // 속성은 '_'로 시작한다.
         // $Button.addEventListener("click", () => {});
     }
+
+    disconnectedCallback() {
+        // browser calls this method when the element is removed from the document
+        // (can be called many times if an element is repeatedly added/removed)
+    }
+
+    static get observedAttributes() {
+        return [/* array of attribute names to monitor for changes */];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        // called when one of attributes listed above is modified
+    }
+
+    adoptedCallback() {
+        // called when the element is moved to a new document
+        // (happens in document.adoptNode, very rarely used)
+    }
+
+    // there can be other element methods and properties
 }
 
-// Define the new element
-customElements.define('haedo-elem', Content);
+customElements.define("my-element", MyElement);
 
-// // Create a class for the element
-// class ContentCard extends HTMLElement {
-//     constructor() {
-//         // Always call super first in constructor
-//         super();
+/**
+ * 상속 element
+ */
 
-//         // Create a shadow root
-//         var shadow = this.attachShadow({ mode: 'open' });
+class PlaneImage extends HTMLImageElement {
+    constructor() {
+        // 항상 super를 생성자에서 먼저 호출합니다
+        super();
 
-//         // Create spans
-//         var wrapper = document.createElement('span');
-//         wrapper.setAttribute('class', 'wrapper');
-//         var icon = document.createElement('span');
-//         icon.setAttribute('class', 'icon');
-//         icon.setAttribute('tabindex', 0);
-//         var info = document.createElement('span');
-//         info.setAttribute('class', 'info');
+        // 요소 기능은 여기 작성됩니다
 
-//         // Take attribute content and put it inside the info span
-//         var text = this.getAttribute('text');
-//         info.textContent = text;
+        const $imgElem = document.createElement("img");
+        $imgElem.src = "./default.png";
+        $imgElem.alt = " default alt";
+        $imgElem.style.width = 'auto';
+        $imgElem.style.height = '24px';
+    }
+}
 
-//         // Insert icon
-//         var imgUrl;
-//         if (this.hasAttribute('img')) {
-//             imgUrl = this.getAttribute('img');
-//         } else {
-//             imgUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXbNugHKt-xZSOXgn3HoMIs9BXVQiHXf4xmQ&usqp=CAU';
-//         }
-//         var img = document.createElement('img');
-//         img.src = imgUrl;
-//         icon.appendChild(img);
-
-//         // Create some CSS to apply to the shadow dom
-//         var style = document.createElement('style');
-
-//         style.textContent = '.wrapper {' +
-//             'position: relative;' +
-//             '}' +
-
-//             '.info {' +
-//             'font-size: 0.8rem;' +
-//             'width: 200px;' +
-//             'display: inline-block;' +
-//             'border: 1px solid black;' +
-//             'padding: 10px;' +
-//             'background: white;' +
-//             'border-radius: 10px;' +
-//             'opacity: 0;' +
-//             'transition: 0.6s all;' +
-//             'position: absolute;' +
-//             'top: 20px;' +
-//             'left: 10px;' +
-//             'z-index: 3;' +
-//             '}' +
-
-//             'img {' +
-//             'width: 1.2rem' +
-//             '}' +
-
-//             '.icon:hover + .info, .icon:focus + .info {' +
-//             'opacity: 1;' +
-//             '}';
-
-//         // attach the created elements to the shadow dom
-
-//         shadow.appendChild(style);
-//         shadow.appendChild(wrapper);
-//         wrapper.appendChild(icon);
-//         wrapper.appendChild(info);
-//     }
-// }
+customElements.define('plane-image', PlaneImage, { extends: 'img' });
