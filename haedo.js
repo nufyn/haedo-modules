2b891724d2327b3915f4993a6481dab9f9fa247f
Roun <ljw6828@gmail.com>
2022-09-01 12:56:50 +0900

    프론트 모듈 개발을 위한 init

--- haedo.js
+++ haedo.js
@@ -1,3 +1,30 @@
 class Haedo {
+    /**
+     * 기능 설명
+     * @version 1.0 // 버전 명시
+     * @param string id // 
+     * @param string pw
+     * @param string redirect_url
+     */
+    login(id, pw, redirect_url = "/") {
+        $.ajax({
+            // 파라미터로 받는게 좋을까 우리 모듈에서 무조건 지정하는 것이 좋을까
+            url: '/apis/v1/',
+            type: 'POST',
+            dataType: 'json',
+            data: {
+            },
+            success: (res) => {
+                // set cookie 등 로그인 완료 세팅
 
+                // 리다이렉션
+                location.href = redirect_url;
+            },
+            error: (err) => {
+
+            }
+        });
+    }
 }
+
+var haedo = new Haedo();
