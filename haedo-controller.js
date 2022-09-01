5efbfb9e4e8996fdea8f3b003e729610683261d7
Roun <ljw6828@gmail.com>
2022-09-01 23:17:03 +0900

    v.0.0.1 login 결과 없음 이슈 해결 중

--- haedo-controller.js
+++ haedo-controller.js
@@ -2,7 +2,9 @@
  * 본 스크립트는 백엔드 API 입니다.
  * 프로젝트에 적절한 라우팅을 부탁드립니다. 
  * 
- * bycrypt 모듈 설치 부탁드립니다.
+ * 설치 필요 모듈 리스트
+ * mysql
+ * bycrypt
  *  */
 
 const express = require('express');
@@ -27,32 +29,33 @@ module.exports = {
             const data = [req.body.id];
             connection.query(sql, data, (err, results) => {
                 console.log("results", results)
-
-                bcrypt.compare(req.body.pw, results[0].pw, function (err, pwd_confirm) {
-                    if (pwd_confirm) {
-                        res.json({
-                            // Nufyn Error Code. 아래 에러코드를 참고해주세요.
-                            "status": {
-                                "status_code": 200,
-                                "status_msg": null,
-                                "status_err": null
-                            },
-                            // API처리 결과 반환은 아래 results 변수에 담아주세요.
-                            "results": results
-                        })
-                    } else {
-                        res.json({
-                            // Nufyn Error Code. 아래 에러코드를 참고해주세요.
-                            "status": {
-                                "status_code": 501,
-                                "status_msg": "Login 에러입니다.",
-                                "status_err": ERROR
-                            },
-                            // API처리 결과 반환은 아래 results 변수에 담아주세요.
-                            "results": null
-                        })
-                    }
-                });
+                if (results.length != 0) {
+                    bcrypt.compare(req.body.pw, results[0].pw, function (err, pwd_confirm) {
+                        if (pwd_confirm) {
+                            res.json({
+                                // Nufyn Error Code. 아래 에러코드를 참고해주세요.
+                                "status": {
+                                    "status_code": 200,
+                                    "status_msg": null,
+                                    "status_err": null
+                                },
+                                // API처리 결과 반환은 아래 results 변수에 담아주세요.
+                                "results": results
+                            })
+                        } else {
+                            res.json({
+                                // Nufyn Error Code. 아래 에러코드를 참고해주세요.
+                                "status": {
+                                    "status_code": 501,
+                                    "status_msg": "Login 에러입니다.",
+                                    "status_err": ERROR
+                                },
+                                // API처리 결과 반환은 아래 results 변수에 담아주세요.
+                                "results": null
+                            })
+                        }
+                    });
+                }
             })
         })
     }
