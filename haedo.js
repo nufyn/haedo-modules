d00182104c4a012dc6f8dc6c1f3cdbc26015650c
Roun <ljw6828@gmail.com>
2022-09-02 00:40:57 +0900

    Sign v.0.0.1 프론트용 Sign 모듈 init

--- haedo.js
+++ haedo.js
@@ -5,25 +5,33 @@
  */
 class Haedo {
     /**
-     * 기능 설명
+     * 회원 가입
      * @version 1.0 // 버전 명시
      * @param {string} id // 
      * @param {string} pw
      * @param {string} redirect_url
      */
-    login(id, pw, redirect_url = "/") {
+    signin(id, pw, pw_confirm, redirect_url = "/") {
+        if (pw != pw_confirm) {
+            alert("비밀번호를 확인해주세요.")
+            return;
+        }
+
         $.ajax({
             // 파라미터로 받는게 좋을까 우리 모듈에서 무조건 지정하는 것이 좋을까
-            url: '/apis/v1/login',
+            url: '/apis/v1/sign/in',
             type: 'POST',
             dataType: 'json',
             data: {
+                /**
+                 * DB 컬럼에 맞춰 Key-Value를 넘겨주세요
+                 */
                 id: id,
                 pw: pw
             },
             success: (res) => {
                 // set cookie 등 로그인 완료 세팅
-                alert("로그인입니다");
+                console.log("회원가입 성공");
                 console.log(res);
                 // 리다이렉션
                 location.href = redirect_url;
